8ebd392c16699322ea57dcb95303132db88a48ea
327jwolf <327jwolf@gmail.com>
2020-05-26 21:24:39 -0500

    removed imports changed back to commonjs

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
 #!/usr/bin/env node
 
-import express, { static } from 'express';
-import { join } from 'path';
-import logger from 'morgan';
-import cookieParser from 'cookie-parser';
-import { json, urlencoded } from 'body-parser';
-import { sysconfig as config } from './config/appconfig.js';
-import { configPLC as confPLC } from './config/appconfig.js';
+const express = require('express');
+const join = require('path').join;
+const logger = require('morgan').logger;
+const cookieParser = require('cookie-parser');
+const bodyparser = require('body-parser');
+const  config = require('./config/appconfig.js').sysconfig;
+const confPLC = require('./config/appconfig.js').configPLC;
 
 const app = express();
 const server = require('http').createServer(app);
 
 // import nodepccc from 'nodepccc';
-import getPLCInfo from './middleware/getPLCInfo.js';
+const getPLCInfo = require('./middleware/getPLCInfo.js');
 // import logIt from './middleware/logIt.js';
-import { getOmronInfo } from './middleware/node-omron-fins-master/getomroninfo.js';
-import translate2serial from './middleware/translate2serial.js';
-import index from './routes/index';
+const getOmronInfo = require('./middleware/node-omron-fins-master/getomroninfo.js');
+const translate2serial = require('./middleware/translate2serial.js');
+const index = require('./routes/index');
 const port = process.argv[2] || 3030;
 
 //app.use(express.static('public'));
@@ -31,10 +31,10 @@ app.set('env', 'dev');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(json());
-app.use(urlencoded({ extended: false }));
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(static(join(__dirname, 'public')));
+app.use(static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 
